f05c18dac352a8fb06fd8ddd7e6e9fe2ec710466
Simon <simonwatson176@gmail.com>
2021-09-09 11:00:14 +0100

    initial commit

---
+++ src/test/java/com/codeclan/example/bookingService/BookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.bookingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
